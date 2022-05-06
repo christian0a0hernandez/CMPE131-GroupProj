c59dda99097620b4609cbeb1ab9475c62bca8149
Steven <stevenpstansberry@gmail.com>
2022-05-06 16:07:37 -0700

    fixed form fields

--- website/models.py
+++ website/models.py
@@ -4,32 +4,13 @@ from sqlalchemy.sql import func
 from . import db
 
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-class User(db.Model, UserMixin):
-=======
 class User(db.Model, UserMixin): # creates a user
->>>>>>> Stashed changes
-=======
-class User(db.Model, UserMixin): # creates a user
->>>>>>> Stashed changes
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    notes = db.relationship('Note')
-=======
-=======
->>>>>>> Stashed changes
+
 
 class Brand(db.Model): #creates a brand
     id = db.Column(db.Integer, primary_key=True)
@@ -39,7 +20,3 @@ class Category(db.Model): #creates a category
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=True)
 
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
