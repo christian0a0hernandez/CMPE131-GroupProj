f613946b22f8ed2ac75ef4f3288657cf330a6ef8
Steven Paul Stansberry <102209562+stevenpstansberry@users.noreply.github.com>
2022-05-06 22:06:48 -0700

    Update models.py

--- website/models.py
+++ website/models.py
@@ -4,8 +4,18 @@ from sqlalchemy.sql import func
 from . import db
 
 
-class User(db.Model, UserMixin):
+class User(db.Model, UserMixin): # creates a user
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
+
+
+class Brand(db.Model): #creates a brand
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False,unique = True)
+
+class Category(db.Model): #creates a category
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
