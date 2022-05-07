46548773403c683bafea85e0e81311df4620b718
Steven Paul Stansberry <102209562+stevenpstansberry@users.noreply.github.com>
2022-05-06 22:08:13 -0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -11,6 +11,6 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home ():
-    return render_template("home.html", user = current_user)
+    return render_template("home.html",user = current_user)
 
 
